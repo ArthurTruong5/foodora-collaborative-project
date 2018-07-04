9f03c72efc68531975ce84cc7ca48ed8d7b619df
Jesse Madison <jessemadison@hotmail.com>
2018-07-04 15:27:13 +1000

    new commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,23 @@
 class User < ApplicationRecord
   has_one :restaurant
   has_one :customer
-  # dependant/destroy will delete the users comment if they delete their account to avoid conflict 
+  # dependant/destroy will delete the users comment if they delete their account to avoid conflict
    has_many :reviews, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+
+         def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.name = auth.info.name   # assuming the user model has a name
+    user.image = auth.info.image # assuming the user model has an image
+    # If you are using confirmable and the provider(s) you use validate emails,
+    # uncomment the line below to skip the confirmation emails.
+    # user.skip_confirmation!
+  end
+end
+
 end
